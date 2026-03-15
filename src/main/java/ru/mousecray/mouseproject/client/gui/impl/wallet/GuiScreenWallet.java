2c2fb7d059028755e271b2c3f043206a40104b37
ruden <kucheryawiy.roman@yandex.ru>
2026-03-15 21:10:16 +0300

    Update license text in files

--- src/main/java/ru/mousecray/mouseproject/client/gui/impl/wallet/GuiScreenWallet.java
+++ src/main/java/ru/mousecray/mouseproject/client/gui/impl/wallet/GuiScreenWallet.java
@@ -1,7 +1,7 @@
-/*
+/*******************************************************************************
  * Copyright © 2026 mousecray
  * Licensed under the GNU Lesser General Public License, Version 3.0
- */
+ ******************************************************************************/
 
 package ru.mousecray.mouseproject.client.gui.impl.wallet;
 
